4f8b75ad3c0ed48cf3adc03fb82086af7ed2ccd0
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-01-04 16:21:05 +0100

    Remove unused packages.

--- src/main/java/dk/aau/cs/loader/SSBLoader.java
+++ src/main/java/dk/aau/cs/loader/SSBLoader.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.jena.query.Dataset;
