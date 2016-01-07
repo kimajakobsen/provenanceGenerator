a8fd67f4e754e571cf4083c3f73d1d4cab2f5304
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-01-07 10:38:11 +0100

    Change the way models are stores before they are added as named graphs.

--- src/main/java/dk/aau/cs/loader/AbstractLoader.java
+++ src/main/java/dk/aau/cs/loader/AbstractLoader.java
@@ -1,6 +1,7 @@
 package dk.aau.cs.loader;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.jena.ext.com.google.common.collect.HashMultimap;
@@ -10,12 +11,7 @@ import org.apache.jena.rdf.model.Model;
 public abstract class AbstractLoader {
 	
 	protected List<String> files = new ArrayList<String>();
-	// Emptystring is default graph
-	protected Multimap<String, Model> models = HashMultimap.create();
-
-//	public Model getModel() {
-//		return model;
-//	}
+	private HashMap<String, Model> models = new HashMap<String,Model>();
 
 	public List<String> getFilePaths() {
 		return files;
@@ -27,6 +23,19 @@ public abstract class AbstractLoader {
 	
 	public AbstractLoader(List<String> files) {
 		this.files = files;
+	}
+	
+	public HashMap<String,Model> getModelContainer() {
+		return models;
+	}
+	
+	public void insertIntoModelContainer(String graph, Model model) {
+		if (models.containsKey(graph)) {
+			Model temp = models.get(graph).add(model);
+			models.put(graph, temp);
+		} else {
+			models.put(graph, model);
+		}
 	}
 	
 	public abstract void run(boolean b);
