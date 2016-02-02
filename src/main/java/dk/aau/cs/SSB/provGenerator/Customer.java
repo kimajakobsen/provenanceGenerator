efadb3667af0cbdeb51c044c30b1af59edf98bd9
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-02-02 11:57:25 +0100

    Clean up and added actors.

--- src/main/java/dk/aau/cs/SSB/provGenerator/Customer.java
+++ src/main/java/dk/aau/cs/SSB/provGenerator/Customer.java
@@ -8,18 +8,16 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Statement;
 
 import dk.aau.cs.prov.Activity;
+import dk.aau.cs.prov.Actor;
 import dk.aau.cs.prov.Cleaner;
 import dk.aau.cs.prov.Entity;
-import dk.aau.cs.prov.ProvenanceEntity;
 import dk.aau.cs.prov.Merge;
+import dk.aau.cs.prov.Organization;
+import dk.aau.cs.prov.Person;
+import dk.aau.cs.prov.ProvenanceEntity;
 import dk.aau.cs.prov.Source;
 
 public class Customer extends ProvGenerator {
-
-//	private ArrayList<String> sources = new ArrayList<String>();
-//	private ArrayList<String> cleaners = new ArrayList<String>();
-//	private ArrayList<String> actors = new ArrayList<String>();
-//	private ArrayList<String> mergers = new ArrayList<String>();
 	
 	Pair<LocalDateTime, LocalDateTime> level1 = IntervalManager.getIntervalLevel(1);
 	Pair<LocalDateTime, LocalDateTime> level2 = IntervalManager.getIntervalLevel(2);
@@ -28,40 +26,27 @@ public class Customer extends ProvGenerator {
 	String provenanceIdentifier;
 	
 	public Customer() {
-//		sources.add("prDepartment");
-//		sources.add("accountingDepartment");
-//		sources.add("Web form");
-//		sources.add("excelFile");
-//		sources.add("Public register");O
-//		sources.add("Sales department");
-//		sources.add("Database");
-//		
-//		cleaners.add("removeDoubleQuotes");
-//		cleaners.add("toLowerCase");
-//		cleaners.add("removeLocalizedCharecters");
-//		
-//		mergers.add("");
-//		mergers.add("");
-//		mergers.add("");
 		
 		model = ModelFactory.createDefaultModel();
-		//TODO add actor
 		
-		Entity source1 = new Source();
-		Entity source2 = new Source();
-		Entity source3 = new Source();
+		Actor actor1 = new Person();
+		Actor actor2 = new Organization();
 		
-		Activity cleaner1 = new Cleaner(source1,level1);
+		Entity source1 = new Source(actor2);
+		Entity source2 = new Source(actor2);
+		Entity source3 = new Source(actor2);
+		
+		Activity cleaner1 = new Cleaner(source1,level1,actor1);
 		Entity entity1 = new ProvenanceEntity(cleaner1);
-		Activity cleaner2 = new Cleaner(entity1,level2);
+		Activity cleaner2 = new Cleaner(entity1,level2,actor1);
 		Entity entity2 = new ProvenanceEntity(cleaner2);
 		
-		Activity cleaner3 = new Cleaner(source2,level1);
+		Activity cleaner3 = new Cleaner(source2,level1,actor1);
 		Entity entity3 = new ProvenanceEntity(cleaner3);
-		Activity merge1 = new Merge(entity3, source3,level2);
+		Activity merge1 = new Merge(entity3, source3,level2,actor1);
 		Entity entity4 = new ProvenanceEntity(merge1);
 		
-		Activity merge2 = new Merge(entity4, entity2,level3);
+		Activity merge2 = new Merge(entity4, entity2,level3,actor1);
 		Entity entity5 = new ProvenanceEntity(merge2);
 		provenanceIdentifier = entity5.getSubject().toString();
 		
@@ -79,14 +64,8 @@ public class Customer extends ProvGenerator {
 		model.add(entity5.createModel());
 		model.add(merge1.createModel());
 		model.add(merge2.createModel());
-		
-		
-		
-		
-		
-		
-		
-		
+		model.add(actor1.createModel());
+		model.add(actor2.createModel());
 		
 	}
 	
