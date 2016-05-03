5df130f6e4452dac71a1b2a3cfc786c5e7c0df2b
Kim A. Jakobsen <kimajakobsen@gmail.com>
2016-05-03 14:43:30 +0200

    Fixed a bug where the level members was given the wrong name.

--- src/main/java/dk/aau/cs/SSB/schema/Lineorder.java
+++ src/main/java/dk/aau/cs/SSB/schema/Lineorder.java
@@ -17,7 +17,7 @@ public class Lineorder extends Schema {
 		schema.put(2, new SchemaPropertySet("custkey", 			"customer", false,	"int"));
 		schema.put(3, new SchemaPropertySet("partkey", 			"part", 	false,	"int"));
 		schema.put(4, new SchemaPropertySet("suppkey", 			"supplier", false,	"int"));
-		schema.put(5, new SchemaPropertySet("orderdate", 		"orderdate", 	false,	"int"));
+		schema.put(5, new SchemaPropertySet("orderdate", 		"date", 	false,	"int"));
 		schema.put(6, new SchemaPropertySet("orderpriority", 	"", 		false,	"str"));
 		schema.put(7, new SchemaPropertySet("shippriority", 	"", 		false,	"int"));
 		schema.put(8, new SchemaPropertySet("quantity",			"", 		false,	"int"));
@@ -27,7 +27,7 @@ public class Lineorder extends Schema {
 		schema.put(12, new SchemaPropertySet("revenue",			"", 		false,	"int"));
 		schema.put(13, new SchemaPropertySet("supplycost",		"", 		false,	"int"));
 		schema.put(14, new SchemaPropertySet("tax",				"", 		false,	"int"));
-		schema.put(15, new SchemaPropertySet("commitdate",		"commitdate", 	false,	"int"));
+		schema.put(15, new SchemaPropertySet("commitdate",		"date", 	false,	"int"));
 		schema.put(16, new SchemaPropertySet("shipmode",		"", 		false,	"str"));
 	}
 
